7c10f1803fe738de244bc1a3e91fd54bd4ea8d3f
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-10 19:27:09 +0900

    refactor(auth-service): 유저 생성 시 nickname 값 받도록 추가

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,9 +2,11 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @NoArgsConstructor
@@ -17,4 +19,8 @@ public class SignupRequest {
     private String password;
     @NotBlank
     private String userRole;
+
+    @NotBlank(message = "닉네임을 입력해주세요")
+    @Size(min = 1, max = 12, message = "닉네임은 1자 이상 12자 이하여야 합니다")
+    private String nickname;
 }
