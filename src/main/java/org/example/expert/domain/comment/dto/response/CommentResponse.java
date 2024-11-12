095b73d8ba19e65ef04810125d3d76a9658f93fa
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-12 17:50:07 +0900

    feat(comment-dto): 생성자 프로젝션 준비 및 코멘트 조회 시 페이징 수행하기 위한 CommentListRespDto 추가

--- src/main/java/org/example/expert/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/org/example/expert/domain/comment/dto/response/CommentResponse.java
@@ -1,2 +1,27 @@
-package org.example.expert.domain.comment.dto.response;public class CommentResponse {
+package org.example.expert.domain.comment.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.expert.domain.comment.entity.Comment;
+import org.example.expert.domain.user.dto.response.UserResponse;
+
+@NoArgsConstructor
+@Getter
+public class CommentResponse {
+
+    private Long id;
+    private String contents;
+    private UserResponse user;
+
+    public CommentResponse(Long id, String contents, UserResponse user) {
+        this.id = id;
+        this.contents = contents;
+        this.user = user;
+    }
+
+    public CommentResponse(Comment comment) {
+        this.id = comment.getId();
+        this.contents = comment.getContents();
+        this.user = new UserResponse(comment.getUser());
+    }
 }
--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -1,13 +1,20 @@
 package org.example.expert.domain.user.dto.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.expert.domain.user.entity.User;
 
 @Getter
+@NoArgsConstructor
 public class UserResponse {
 
-    private final Long id;
-    private final String email;
+    private Long id;
+    private String email;
+
+    public UserResponse(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
 
     public UserResponse(User user) {
         this.id = user.getId();
