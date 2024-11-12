88b760d56edc3b3c4abbdc8d4c01aa42c4df03e8
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-12 17:52:07 +0900

    feat(comment-controller): 페이징 정보 받도록 추가

--- src/main/java/org/example/expert/domain/comment/controller/CommentController.java
+++ src/main/java/org/example/expert/domain/comment/controller/CommentController.java
@@ -1,20 +1,26 @@
 package org.example.expert.domain.comment.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
+import org.example.expert.domain.comment.dto.response.CommentListRespDto;
 import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.service.CommentService;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class CommentController {
 
     private final CommentService commentService;
@@ -29,7 +35,9 @@ public class CommentController {
     }
 
     @GetMapping("/todos/{todoId}/comments")
-    public ResponseEntity<List<CommentResponse>> getComments(@PathVariable(value = "todoId") Long todoId) {
-        return ResponseEntity.ok(commentService.getComments(todoId));
+    public ResponseEntity<CommentListRespDto> getComments(@PathVariable(value = "todoId") Long todoId,
+                                                          @RequestParam(value = "page", defaultValue = "0", required = false) @PositiveOrZero Integer page,
+                                                          @RequestParam(value = "size", defaultValue = "10", required = false) @Positive Integer size) {
+        return ResponseEntity.ok(commentService.getComments(todoId, page, size));
     }
 }
