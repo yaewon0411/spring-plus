8838b52924824b81d1e7ea7d42d6ac3b8fc105bc
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-12 18:25:29 +0900

    test(comment-service): 댓글 목록 조회 단위 테스트 추가

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.comment.service;
 import org.example.expert.domain.comment.dto.response.CommentListRespDto;
 import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.repository.CommentRepository;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.service.TodoService;
 import org.example.expert.domain.user.dto.response.UserResponse;
@@ -12,17 +13,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
@@ -71,5 +69,87 @@ class CommentServiceTest {
         );
 
     }
+
+    @Test
+    @DisplayName("todo 댓글 목록 페이징 조회 성공 테스트: 댓글이 없는 경우")
+    void getComments_success_noComments(){
+        Long todoId = 1L;
+        int page = 0;
+        int size = 10;
+
+        Todo todo = new Todo("title","contents","weather", null);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "modifiedAt"));
+        Page<CommentResponse> emptyPage = new PageImpl<>(List.of(), pageable, 0);
+
+        when(todoService.findByIdOrFail(todoId)).thenReturn(todo);
+        when(commentRepository.getCommentsWithUserByTodoId(todoId, pageable))
+                .thenReturn(emptyPage);
+
+        //when
+        CommentListRespDto result = commentService.getComments(todoId, page, size);
+
+        //then
+        assertAll(
+                () -> assertThat(result.getCommentList()).isEmpty(),
+                () -> assertThat(result.getTotalElements()).isZero(),
+                () -> assertThat(result.getTotalPages()).isZero(),
+                () -> assertThat(result.getPageNumber()).isZero(),
+                () -> assertThat(result.isHasNext()).isFalse(),
+                () -> assertThat(result.isHasPrevious()).isFalse()
+        );
+
+    }
+
+    @Test
+    @DisplayName("todo 댓글 목록 페이징 조회 성공 테스트: 페이지 번호가 총 페이지 수 초과하면 빈 목록 반환")
+    void getComments_success_pageNumberExceeded() {
+        Long todoId = 1L;
+        int page = 2;
+        int size = 10;
+        int totalCount = 12;
+        Todo todo = new Todo("title","contents","weather", null);
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "modifiedAt"));
+        Page<CommentResponse> emptyPage = new PageImpl<>(List.of(), pageable, totalCount);
+
+        when(todoService.findByIdOrFail(todoId)).thenReturn(todo);
+        when(commentRepository.getCommentsWithUserByTodoId(todoId, pageable))
+                .thenReturn(emptyPage);
+
+        // when
+        CommentListRespDto result = commentService.getComments(todoId, page, size);
+
+        // then
+        assertAll(
+                () -> assertThat(result.getCommentList()).isEmpty(),
+                () -> assertThat(result.getTotalElements()).isEqualTo(totalCount),
+                () -> assertThat(result.getTotalPages()).isEqualTo(2),
+                () -> assertThat(result.getPageNumber()).isEqualTo(2),
+                () -> assertThat(result.isHasNext()).isFalse(),
+                () -> assertThat(result.isHasPrevious()).isTrue()
+        );
+    }
+
+    @Test
+    @DisplayName("todo 댓글 목록 페이징 조회 실패 테스트: 존재하지 않는 할일")
+    void getComments_TodoNotFound() {
+        Long todoId = 1L;
+        int page = 0;
+        int size = 10;
+        String todoNotFound = "Todo Not Found";
+
+        doThrow(new InvalidRequestException(todoNotFound)).when(todoService).findByIdOrFail(todoId);
+
+        // when
+        assertThatThrownBy(() ->
+                commentService.getComments(todoId, page, size))
+                .isInstanceOf(InvalidRequestException.class)
+                .hasMessage(todoNotFound);
+
+        verify(todoService).findByIdOrFail(todoId);
+        verify(commentRepository, never()).getCommentsWithUserByTodoId(any(), any());
+    }
+
+
 
 }
