ce7c01407ef88084ae1114fe8dc4c76dba7f9954
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-13 17:06:29 +0900

    feat(todo-controller): 검색 api 추가

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -3,21 +3,22 @@ package org.example.expert.domain.todo.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.config.security.loginuser.LoginUser;
-import org.example.expert.domain.common.annotation.Auth;
-import org.example.expert.domain.common.dto.AuthUser;
-import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.request.TodoSearchReqDto;
+import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.request.TodoListReqDto;
 import org.example.expert.domain.todo.dto.response.TodoListRespDto;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchRespDto;
 import org.example.expert.domain.todo.service.TodoService;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class TodoController {
 
     private final TodoService todoService;
@@ -31,13 +32,18 @@ public class TodoController {
     }
 
     @GetMapping("/todos")
-    public ResponseEntity<TodoListRespDto> getTodos(@Valid TodoSearchReqDto todoSearchReqDto
+    public ResponseEntity<TodoListRespDto> getTodos(@Valid TodoListReqDto todoListReqDto
     ) {
-        return ResponseEntity.ok(todoService.getTodos(todoSearchReqDto));
+        return ResponseEntity.ok(todoService.getTodos(todoListReqDto));
     }
 
     @GetMapping("/todos/{todoId}")
     public ResponseEntity<TodoResponse> getTodo(@PathVariable(value = "todoId") Long todoId) {
         return ResponseEntity.ok(todoService.getTodo(todoId));
+    }
+
+    @GetMapping("/todos/search")
+    public ResponseEntity<TodoSearchRespDto> searchTodos(@Valid TodoSearchReqDto todoSearchReqDto){
+        return ResponseEntity.ok(todoService.searchTodos(todoSearchReqDto));
     }
 }
