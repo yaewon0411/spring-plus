5c15934cde2ff0ceae46ead596db45c57eb0f917
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-15 20:32:30 +0900

    feat(manager): Manager.java -> Managaer.kt 생성 및 적용

--- src/main/java/org/example/expert/domain/manager/entity/Manager.kt
+++ src/main/java/org/example/expert/domain/manager/entity/Manager.kt
@@ -1,4 +1,37 @@
 package org.example.expert.domain.manager.entity
 
-class Manager {
+import jakarta.persistence.*
+import org.example.expert.domain.common.entity.KTimestamped
+import org.example.expert.domain.common.exception.InvalidRequestException
+import org.example.expert.domain.todo.entity.Todo
+import org.example.expert.domain.user.entity.User
+
+@Entity
+@Table(name = "managers")
+class Manager protected constructor(
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    var user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "todo_id", nullable = false)
+    var todo: Todo,
+): KTimestamped(){
+
+    constructor(user: User, todo: Todo) :this(
+        id = null,
+        user = user,
+        todo = todo
+    )
+
+    fun isAssigned(todoId: Long) {
+        if (todo.id != todoId) {
+            throw InvalidRequestException("해당 일정에 등록된 담당자가 아닙니다.")
+        }
+    }
+
 }
